08bc5085909e801c41e6a1bfbd9051293859ed1a
Touseef Ahmed <touseefahmed0707@gmail.com>
2025-06-17 15:00:32 +0400

    nothing

--- nextjs-main/app/page.tsx
+++ nextjs-main/app/page.tsx
@@ -1,12 +1,9 @@
 import Image from "next/image";
-import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
     <div>
-      <Navbar/>
-      
-      <h1>Hello Afzar World</h1>
+      <h1>Welcome to Afzar Hydraulics</h1>
     </div>
   );
 }
