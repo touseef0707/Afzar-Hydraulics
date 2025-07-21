7da9a61e508e178e22711758f834999736d834a3
Touseef Ahmed <touseefahmed0707@gmail.com>
2025-07-21 16:48:47 +0400

    minor fix

--- nextjs-main/firebase/firebase-admin.ts
+++ nextjs-main/firebase/firebase-admin.ts
@@ -6,7 +6,7 @@ const adminApp = getApps()[0] || initializeApp({
   credential: cert({
     projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
     clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY,
   }),
 });
 
